b4c6b32f99d72893be3141e33b7ab35e576ff618
DiegoG0477 <diego.14.13.lopez@gmail.com>
2024-11-28 02:40:23 -0600

    ui added

--- src/main/java/com/simulation/restaurant/App.java
+++ src/main/java/com/simulation/restaurant/App.java
@@ -1,8 +1,58 @@
 package com.simulation.restaurant;
 
-public class App {
-    public static void main(String[] args) {
+import java.util.List;
+import com.simulation.restaurant.infrastructure.ui.scene.Entrance;
+import com.simulation.restaurant.infrastructure.ui.scene.Kitchen;
+import com.simulation.restaurant.infrastructure.ui.scene.Reception;
+import com.simulation.restaurant.infrastructure.ui.scene.Table;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+
+public class App extends GameApplication {
+
+    private List<Entity> tables;
+    private Entity kitchen;
+    private Entity reception;
+    private Entity entrance;
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("Restaurant Simulation");
+    }
+
+    @Override
+    protected void initGame() {
+        FXGL.getGameScene().setBackgroundRepeat("floor.png");
+
+        // Create the entrance
+        entrance = Entrance.create();
+        FXGL.getGameWorld().addEntity(entrance);
+
+        // Create the reception
+        reception = Reception.create();
+        FXGL.getGameWorld().addEntity(reception);
+
+        // Create the kitchen
+        kitchen = Kitchen.create();
+        FXGL.getGameWorld().addEntity(kitchen);
+
+        // Create 9 tables
+        tables = Table.createTables(5, 4, 350, 50);
+        tables.forEach(FXGL.getGameWorld()::addEntity);
+    }
+
+    private void initializeAndSimulateRestaurant() {
         Restaurante restaurante = new Restaurante(20, 2, 2, 100); // Capacidad, meseros, cocineros, cantidad de comensales a generar
         restaurante.simular();
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.initializeAndSimulateRestaurant(); // Inicializar y simular el restaurante
+        launch(args);
     }
 }
